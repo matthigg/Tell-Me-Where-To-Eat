c2a0e1a8d1a2a29c1a7a7039e2fa1eeb4dfd01a5
matthigg <mhh129@gmail.com>
2019-08-30 00:52:20 -0400

    Add custom javascript to handle form, including event listeners

--- src/components/InputLocationBar.jsx
+++ src/components/InputLocationBar.jsx
@@ -27,26 +27,27 @@ class InputLocationBar extends Component {
         <h1 className="headline">
           Tell Me Where To <span className="headline-emphasis">EAT</span>
         </h1>
-        <span className="under-construction">This site is under construction. Check us out later though!</span>
-        <form action='' method="POST">
-          <div className="input-location-text-wrapper">
-            <input 
-              className="input-location-text-field" 
-              name="input-location-text-field" 
-              type="text" 
-              placeholder="Enter City + State, or Zip Code" 
-              value={ this.state.value } 
-              onChange={ this.handleChange }
-            />
-            <label htmlFor="input-location-text-field" className="input-location-floating-placeholder">Enter City + State, or Zip Code</label>
-          </div>
-          <label className="input-location-gps">
-            <input className="input-location-gps-checkbox" type="checkbox" />
-            <span className="input-location-gps-checkmark"></span>
-            <span className="input-location-gps-text">Use My Device's Location Services Instead</span>
-          </label>
-          <button className="input-location-submit-button" type="Submit" value="submit">GO</button>            
-        </form>
+        <span className="under-construction">This site is under construction. Check us out later though!</span><br />
+        <div className="input-error">
+          <span>Give us a location and we'll tell you where to eat!</span>
+        </div>
+        <div className="input-location-text-wrapper">
+          <input 
+            className="input-location-text-field" 
+            name="input-location-text-field" 
+            type="text" 
+            placeholder="Enter City + State, or Zip Code" 
+            value={ this.state.value } 
+            onChange={ this.handleChange }
+          />
+          <label htmlFor="input-location-text-field" className="input-location-floating-placeholder">Enter City + State, or Zip Code</label>
+        </div>
+        <label className="input-location-gps">
+          <input className="input-location-gps-checkbox" type="checkbox" />
+          <span className="input-location-gps-checkmark"></span>
+          <span className="input-location-gps-text">Use My Device's Location Services Instead</span>
+        </label>
+        <button className="input-location-submit-button" type="Submit" value="submit">GO</button>            
       </div>
     )
   }
--- src/js/inputLocationBar.js
+++ src/js/inputLocationBar.js
@@ -1,8 +1,55 @@
 export default function inputLocationBar() {
 
+  // Hide the "Use my device's location services..." GPS checkbox if
+  // navigator.geolocation is not found
+  if ('geolocation' in navigator === false) {
+    document.querySelector('.input-location-gps').style.display = 'none'
+  }
+
+  // Give the <input> location text field focus if the placeholder is
+  // clicked
   const placeholder = document.querySelector('.input-location-floating-placeholder')
   placeholder.addEventListener('click', () => {
     document.querySelector('.input-location-text-field').focus()
   })
 
+  // Click the submit button if user hits "Enter" while <input> field
+  // has focus, or if the GPS button is checked
+  document.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+      document.querySelector('.input-location-submit-button').click()
+    }
+  })
+
+  // Gather user input for submission
+  const input = document.querySelector('.input-location-text-field')
+  const submit_button = document.querySelector('.input-location-submit-button')
+  const gps_button = document.querySelector('.input-location-gps-checkbox')  
+  submit_button.addEventListener('click', () => {
+
+    // Get user location from their phone/computer
+    // https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
+    if (gps_button.checked) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        console.log(position.coords.latitude, position.coords.longitude);
+        makeGoogleAPIRequest(position)
+      });
+    }
+
+    // Show error message about no value submitted
+    else if (input.value === '') {
+      document.querySelector('.input-error').style.opacity = 1
+    }
+
+    // Get user location from <input>
+    else {
+      const inputtext = input.value
+      makeGoogleAPIRequest(inputtext)
+    }
+  })
+
+  // Send request to Google API
+  function makeGoogleAPIRequest(user_location) {
+    console.log(user_location)
+  }
 }
