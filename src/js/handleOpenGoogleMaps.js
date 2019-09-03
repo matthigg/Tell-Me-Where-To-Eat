e3b1542e135d52646870f02de709f197d4e94fcf
matthigg <mhh129@gmail.com>
2019-09-03 17:26:16 -0400

    Add google analytics event tracking

--- src/js/handleOpenGoogleMaps.js
+++ src/js/handleOpenGoogleMaps.js
@@ -2,6 +2,13 @@ export default function handleOpenGoogleMaps() {
 
   const open_google_maps_button = document.querySelector('.open-google-maps-button')
   open_google_maps_button.addEventListener('click', () => {
+    
+    // Google Analytics
+    window.gtag('event', 'submit', {
+      'event_label': 'Open Google Maps',
+      'value': 1
+    })
+    
     const address = document.querySelector('.search-results-address').innerHTML
     const url = 'https://maps.google.com?q=' + address
     const blank = '_blank'
--- src/js/handleSubmit.js
+++ src/js/handleSubmit.js
@@ -1,5 +1,11 @@
 export default function handleSubmit() {
 
+    // Google Analytics Test
+    window.gtag('event', 'submit', {
+      'event_label': 'Testing if gtag is firing correctly',
+      'value': 1
+    })
+
   // Click the "GO"/submit button if user hits "Enter" 
   document.addEventListener('keyup', (e) => {
     if (e.keyCode === 13) {
@@ -36,6 +42,13 @@ export default function handleSubmit() {
 
   // Send request to Google Geocoding API
   function reverseGeocodingRequest(user_coordinates) {
+
+    // Google Analytics
+    window.gtag('event', 'submit', {
+      'event_label': 'Use reverse geocoding',
+      'value': 1
+    })
+
     const lat = user_coordinates.latitude
     const long = user_coordinates.longitude
     const api_request = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat + ',' + long + '&key=' + process.env.REACT_APP_GEOCODING_API_KEY
@@ -44,7 +57,8 @@ export default function handleSubmit() {
         return response.json()
       })
       .then(function(response_json) {
-        const address = response_json['results'][0]['formatted_address']
+        const address_arr = response_json['results'][0]['formatted_address'].split(', ')
+        const address = address_arr[1] + ' ' + address_arr[2]
         findPlacesRequest(address)
       })
   }
@@ -56,18 +70,27 @@ export default function handleSubmit() {
     const random_1 = Math.floor(Math.random() * restaurant_categories.length)
     const random_2 = Math.floor(Math.random() * restaurant_styles.length)
     const restaurant_type = restaurant_styles[random_2] + ' ' + restaurant_categories[random_1] + ' '
-    input = restaurant_type + 'restaurants in ' + input
+    const search_query = restaurant_type + 'restaurants near ' + input
+    console.log(search_query)
     const google = window.google
     const zero = new google.maps.LatLng(0, 0)
     const map = new google.maps.Map(
       document.querySelector('.map'), {zoom: 18, center: zero})
     const request = {
-      query: input,
+      query: search_query,
       fields: ['formatted_address', 'geometry', 'name', 'permanently_closed']
     }
+    console.log(request)
     const service = new google.maps.places.PlacesService(map);
     service.findPlaceFromQuery(request, function(results, status) {
       if (status === google.maps.places.PlacesServiceStatus.OK && results[0].permanently_closed === undefined) {
+        
+        // Google Analytics
+        window.gtag('event', 'submit', {
+          'event_label': 'Successful search',
+          'value': 1
+        })
+        
         const address = results[0].formatted_address
         const name = results[0].name
         document.querySelector('.search-results-address').innerHTML = address
@@ -77,6 +100,13 @@ export default function handleSubmit() {
         marker.setMap(map)
         openModal()
       } else {
+
+        // Google Analytics
+        window.gtag('event', 'submit', {
+          'event_label': 'Successful search, API quota limit reached',
+          'value': 1
+        })
+
         const url = 'https://www.google.com/search?q=' + input
         const blank = '_blank'
         window.open(`${url}`, `${blank}`, "noopener,noreferrer")
